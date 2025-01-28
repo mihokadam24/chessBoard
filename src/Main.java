747a1be2fe64dc967c28d1e95a5b6301aa0b5f8f
Felhasznalo <mihokadam24@gmail.com>
2025-01-28 11:43:23 +0100

    Implemented with function

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,10 @@ public class Main {
             W B W B W B W B
             B W B W B W B W
          */
+        chessTableDesigner1();
+    }
+
+    public static void chessTableDesigner1() {
         for (int j = 1; j <= 4; j++) {
             for (int i = 1; i <= 8; i++) {
                 if (i % 2 != 0) {
