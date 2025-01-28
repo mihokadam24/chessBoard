79444920fd5dab4f71e0913323c9ee83bba8f75a
Felhasznalo <mihokadam24@gmail.com>
2025-01-28 11:40:32 +0100

    Implemented with spaghetti code

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,23 @@ public class Main {
             W B W B W B W B
             B W B W B W B W
          */
+        for (int j = 1; j <= 4; j++) {
+            for (int i = 1; i <= 8; i++) {
+                if (i % 2 != 0) {
+                    System.out.print("W ");
+                } else {
+                    System.out.print("B ");
+                }
+            }
+            System.out.println();
+            for (int i = 1; i <= 8; i++) {
+                if (i % 2 == 0) {
+                    System.out.print("W ");
+                } else {
+                    System.out.print("B ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
