04f9144086f8a8bfd67c9f423ec0692216313c82
Felhasznalo <mihokadam24@gmail.com>
2025-01-28 11:48:40 +0100

    Supplemented with header and sequence number display

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,11 @@ public class Main {
             B W B W B W B W
          */
         chessTableDesigner1();
+        chessTableDesigner2();
     }
 
     public static void chessTableDesigner1() {
+        System.out.println("Sakktábla rajzoló - 1. változat");
         for (int j = 1; j <= 4; j++) {
             for (int i = 1; i <= 8; i++) {
                 if (i % 2 != 0) {
@@ -33,5 +35,40 @@ public class Main {
             }
             System.out.println();
         }
+        System.out.println("-------------------------------");
     }
+
+    public static final String headerFooter = "  a b c d e f g h";
+
+    public static void chessTableDesigner2() {
+        System.out.println("Sakktábla rajzoló - 2. változat");
+        System.out.println(headerFooter);
+        byte rowMaker = 1;
+        for (int j = 1; j <= 4; j++) {
+            System.out.print(rowMaker + " ");
+            for (int i = 1; i <= 8; i++) {
+                if (i % 2 != 0) {
+                    System.out.print("W ");
+                } else {
+                    System.out.print("B ");
+                }
+            }
+            System.out.println(rowMaker);
+            rowMaker++;
+            // következő sor
+            System.out.print(rowMaker + " ");
+            for (int i = 1; i <= 8; i++) {
+                if (i % 2 == 0) {
+                    System.out.print("W ");
+                } else {
+                    System.out.print("B ");
+                }
+            }
+            System.out.println(rowMaker);
+            rowMaker++;
+        }
+        System.out.println(headerFooter);
+        System.out.println("-------------------------------");
+    }
+
 }
