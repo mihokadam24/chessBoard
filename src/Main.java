9adcaeeaf7a9be3cd77dbf865b758f6dc03d1a7f
Felhasznalo <mihokadam24@gmail.com>
2025-01-28 11:38:07 +0100

    Add task specification

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        /* Feladat: sakktábla mezőinek megjelenítése a konzolon az alábbi mintának megfeleleően
+            W B W B W B W B
+            B W B W B W B W
+            W B W B W B W B
+            B W B W B W B W
+            W B W B W B W B
+            B W B W B W B W
+            W B W B W B W B
+            B W B W B W B W
+         */
     }
 }
